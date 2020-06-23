ba69dba046093d8bc4b3de358bd44b1eea39c503
georgina puig <georginaxcodes@gmail.com>
2020-06-23 11:52:47 -0500

    chatroom relationship

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,5 @@
 class Chatroom < ApplicationRecord
   belongs_to :booking
   has_many :messages
+  has_many :users through :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   has_many :offers
   has_many :reviews
   has_many :bookings
