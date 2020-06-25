a5f5add9f916a69d345aa67193da740b2a0aa899
Juris Roganovs <juris.roganovs@francity.com>
2020-06-26 02:26:27 +0300

    markers added

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,11 @@
 import mapboxgl from 'mapbox-gl';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -9,7 +15,16 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
+      const markers = JSON.parse(mapElement.dataset.markers);
+      markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
+  fitMapToMarkers(map, markers);
   }
 };
 
 export { initMapbox };
+
