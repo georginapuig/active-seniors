1fa5e76f01bbae8e59da3e01f0094f89012da585
georgina puig <georginaxcodes@gmail.com>
2020-06-26 16:46:00 -0500

    fixed bugs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { initAutocomplete } from '..plugins/init_autocomplete.js';
+import { initAutocomplete } from '../plugins/init_autocomplete.js';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
