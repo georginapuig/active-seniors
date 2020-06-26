44183fc3dc2d644712dd78325e4c954035e5a620
georgina puig <georginaxcodes@gmail.com>
2020-06-26 15:48:50 -0500

    fixed map, show page

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -47,30 +47,29 @@ const initMapboxShow = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'show-map',
-      style: 'mapbox://styles/georginap/ckblx6u280v7b1ip5v4hwydzk'
+      style: 'mapbox://styles/mapbox/streets-v10'
     });
 
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }));
+     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
 
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+      const markers = JSON.parse(mapElement.dataset.markers);
+        markers.forEach((marker) => {
+          const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
-      // Create a HTML element for your custom marker
-      const element = document.createElement('div');
-      element.className = 'marker';
-      element.style.backgroundImage = `url('${marker.image_url}')`;
-      element.style.backgroundSize = 'contain';
-      element.style.width = '25px';
-      element.style.height = '25px';
+        const element = document.createElement('div');
+        element.className = 'marker';
+        element.style.backgroundImage = `url('${marker.image_url}')`;
+        element.style.backgroundSize = 'contain';
+        element.style.width = '25px';
+        element.style.height = '25px';
 
       new mapboxgl.Marker(element)
         .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map)
-        .setPopup(popup); // add this
+        .setPopup(popup)
+        .addTo(map);
     });
-    fitMapToMarkers(map, markers);
+  fitMapToMarkers(map, markers);
   }
 };
 
