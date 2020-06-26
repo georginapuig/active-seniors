8cb13a4c6e3d14c57c60e67a649b8eaf5267f9f5
Juris Roganovs <juris.roganovs@francity.com>
2020-06-26 19:24:50 +0300

    changes in js autocomplete

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,9 +1,9 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('flat_address');
-  if (addressInput) {
-    places({ container: addressInput });
+  const locationInput = document.getElementById('offer_location');
+  if (locationInput) {
+    places({ container: locationInput });
   }
 };
 
