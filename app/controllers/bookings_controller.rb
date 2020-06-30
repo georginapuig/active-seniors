c1a82bd8aae011bcaecce53b5d988172b2b0ae47
Juris Roganovs <juris.roganovs@francity.com>
2020-06-30 17:43:31 +0300

    bookings controller and booking model created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,32 @@
 class BookingsController < ApplicationController
+  before_action :set_offer, only: [:new, :create]
+
   def index
+    @bookings = Booking.all.where(user_id: current_user.id)
+  end
+
+  def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.offer = @offer
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render 'offers/show'
+    end
+  end
+
+  private
+
+  def set_offer
+    @offer = Offer.find(params[:offer_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,19 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :offer
 
+  has_many :reviews
   has_many :chatrooms
+
+  validates :start_date, :end_date, presence: true
+  validates :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+  if end_date < start_date
+    errors.add(:end_date, "must be after the start date")
+  end
+  end
 end
