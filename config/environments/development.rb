8278e0a4c375585db57c97bfb187a3d4243404c8
georgina puig <georginaxcodes@gmail.com>
2020-06-30 11:18:24 -0500

    config letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
   config.active_storage.service = :cloudinary
+  
+  config.action_mailer.delivery_method = :letter_opener
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
