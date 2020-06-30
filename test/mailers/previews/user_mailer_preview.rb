634db9a9179adf521b9dda81746c11fbae65027f
georgina puig <georginaxcodes@gmail.com>
2020-06-30 10:58:54 -0500

    tested user mailer

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,8 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
   def welcome
-    UserMailer.welcome
+    user = User.first
+    UserMailer.welcome(user)
   end
 
 end
