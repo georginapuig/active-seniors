69c534e50d47a8abf9f9180c15cb6a8e960d35c1
Carlos Palomino <palominocp@outlook.com>
2020-07-01 16:53:41 -0500

    dynanmic dropdown menu

---
+++ app/javascript/packs/dynamic_subcategories.js
@@ -0,0 +1,27 @@
+const select_categ = document.getElementById("header-text-input1");
+const select_subcateg = document.getElementById("header-text-input2");
+let arrayoptions = [];
+for (var i=1; i<select_subcateg.length; i++) {
+  let newoption = {
+    value: select_subcateg.options[i].value,
+    parent_id:  select_subcateg.options[i].getAttribute("parent_id"),
+    innerText: select_subcateg.options[i].innerText
+  };
+  arrayoptions.push(newoption);
+}
+select_categ.onchange = function() {
+  console.log(arrayoptions);
+  let value = select_categ.value;
+  let num = select_subcateg.length;
+  for (var i=0; i<num; i++) {
+  	select_subcateg.options.remove(0);
+  }
+  arrayoptions.forEach((option) => {
+  	if (option["parent_id"] === value) {
+  	  var new_option = document.createElement("option");
+	  new_option.text = option["innerText"];
+	  new_option.value = option["value"];
+	  select_subcateg.options.add(new_option);
+  	};
+  });
+};
