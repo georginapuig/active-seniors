c1d11087cf42ef34eaab0a04bcfb24f5ebb56797
georgina puig <georginaxcodes@gmail.com>
2020-07-01 16:57:30 -0500

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_203507) do
+ActiveRecord::Schema.define(version: 2020_07_01_213532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,11 +77,12 @@ ActiveRecord::Schema.define(version: 2020_06_30_203507) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
-    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.bigint "booking_id", null: false
+    t.index ["booking_id"], name: "index_messages_on_booking_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "offers", force: :cascade do |t|
@@ -117,7 +118,7 @@ ActiveRecord::Schema.define(version: 2020_06_30_203507) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.string "first_name"
     t.string "last_name"
     t.string "photo"
@@ -134,7 +135,8 @@ ActiveRecord::Schema.define(version: 2020_06_30_203507) do
   add_foreign_key "chatrooms", "bookings"
   add_foreign_key "checkouts", "offers"
   add_foreign_key "checkouts", "users"
-  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "bookings"
+  add_foreign_key "messages", "users"
   add_foreign_key "offers", "categories"
   add_foreign_key "offers", "categories", column: "subcategory_id"
   add_foreign_key "offers", "users"
