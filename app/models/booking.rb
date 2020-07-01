dcfa32883e2a5310c5fb8a5c277628c7a5148668
georgina puig <georginaxcodes@gmail.com>
2020-07-01 18:17:13 -0500

    booking relations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,8 @@ class Booking < ApplicationRecord
   belongs_to :offer
 
   has_many :reviews
-  has_many :chatrooms
+  has_many :messages
+  # has_many :chatrooms
 
   validates :start_date, :end_date, presence: true
   # validates :end_date_after_start_date
