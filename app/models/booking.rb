b6fce1630155e0229a7a30a43776988fee5cf320
Juris Roganovs <juris.roganovs@francity.com>
2020-07-02 18:38:34 +0300

    moved checkout calculations from controller to booking  model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,12 +7,29 @@ class Booking < ApplicationRecord
   has_many :users, through: :messages
 
   validates :start_date, :end_date, presence: true
+
+  def hours
+    (end_date - start_date) / 3600
+  end
+
+  def subtotal
+    hours * offer.price
+  end
+
+  def vat
+    subtotal * 0.2
+  end
+
+  def grand_total
+    subtotal + vat
+  end
+
   # validates :end_date_after_start_date
 
   # private
 
   # def end_date_after_start_date
-  #   return if end_date.blank? || start_date.blank?
+  #   re turn if end_date.blank? || start_date.blank?
 
   # if end_date < start_date
   #   errors.add(:end_date, "must be after the start date")
