609d21f8c1f5bce28878972089e3bea01e8c624a
Carlos Palomino <palominocp@outlook.com>
2020-07-02 16:24:12 -0500

    Seed first version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
