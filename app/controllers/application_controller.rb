9da2c9fa416862a9b62e32a09f93804585d4df04
georgina puig <georginaxcodes@gmail.com>
2020-07-03 14:01:26 -0500

    added devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :age, :gender])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :photo, :age, :gender])
   end
 
   def default_url_options
