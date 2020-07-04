0a46875ba9d3ba8ee2bb3ab169300ae27c8dfe06
georgina puig <georginaxcodes@gmail.com>
2020-07-04 11:07:20 -0500

    config action

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  config.action_mailer.default_url_options = { host: "active-seniors.herokuapp.com" }
-  config.action_controller.default_url_options = { host: "active-seniors.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://active-seniors.herokuapp.com" }
+  config.action_controller.default_url_options = { host: "https://active-seniors.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
